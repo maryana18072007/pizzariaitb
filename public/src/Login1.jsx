a305d0475adc9e40cf01246371bebcda320dd2a7
maryana18072007 <maryana18072007>
2024-09-17 21:04:35 -0300

    Login

---
+++ public/src/Login1.jsx
@@ -0,0 +1,59 @@
+<html>
+ <head>
+  <title>
+   Login Page
+  </title>
+  <script src="https://unpkg.com/react/umd/react.development.js">
+  </script>
+  <script src="https://unpkg.com/react-dom/umd/react-dom.development.js">
+  </script>
+  <script src="https://unpkg.com/@babel/standalone/babel.js">
+  </script>
+  <script src="https://cdn.tailwindcss.com">
+  </script>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
+
+ </head>
+ <body>
+  <div class="logo">
+   <img alt="Game Legends Logo" height="50" src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-LmQ09WWGIGwOeeA4ArnRw0x5/user-uJPET5fjNenSso8wCETWVNOp/img-i3r04bQJJFwICz6goNqAQt16.png?st=2024-09-17T10%3A30%3A48Z&amp;se=2024-09-17T12%3A30%3A48Z&amp;sp=r&amp;sv=2024-08-04&amp;sr=b&amp;rscd=inline&amp;rsct=image/png&amp;skoid=d505667d-d6c1-4a0a-bac7-5c84a87759f8&amp;sktid=a48cca56-e6da-484e-a814-9c849652bcb3&amp;skt=2024-09-17T00%3A44%3A44Z&amp;ske=2024-09-18T00%3A44%3A44Z&amp;sks=b&amp;skv=2024-08-04&amp;sig=COqZ15roymYfxNSeHTft1Gd2xf5g35KRt5nZKnOFMxw%3D" width="100"/>
+  </div>
+  <div class="login-container">
+   <div class="login-title">
+    LOGIN
+   </div>
+   <div>
+    <label>
+     Email:
+    </label>
+    <input class="input-field" placeholder="Email" type="email"/>
+   </div>
+   <div>
+    <label>
+     Senha:
+    </label>
+    <input class="input-field" placeholder="Senha" type="password"/>
+   </div>
+   <button class="login-button">
+    LOGIN
+   </button>
+   <div class="mt-4">
+    <span>
+     Ainda não tem conta?
+     <a class="register-link" href="#">
+      Cadastre-se
+     </a>
+    </span>
+   </div>
+  </div>
+  <div class="absolute top-1/2 left-0 transform -translate-y-1/2">
+   <img alt="Character Image" class="character" height="50" src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-LmQ09WWGIGwOeeA4ArnRw0x5/user-uJPET5fjNenSso8wCETWVNOp/img-poeWulglYW4aGtkUH8q32RP5.png?st=2024-09-17T10%3A30%3A54Z&amp;se=2024-09-17T12%3A30%3A54Z&amp;sp=r&amp;sv=2024-08-04&amp;sr=b&amp;rscd=inline&amp;rsct=image/png&amp;skoid=d505667d-d6c1-4a0a-bac7-5c84a87759f8&amp;sktid=a48cca56-e6da-484e-a814-9c849652bcb3&amp;skt=2024-09-16T23%3A13%3A38Z&amp;ske=2024-09-17T23%3A13%3A38Z&amp;sks=b&amp;skv=2024-08-04&amp;sig=2DNKCAWK6NJi1zdPliAqi1MGvaxYyPhACDzNqlZmPlw%3D" width="50"/>
+  </div>
+  <div class="absolute top-1/2 right-0 transform -translate-y-1/2">
+   <img alt="Character Image" class="character" height="50" src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-LmQ09WWGIGwOeeA4ArnRw0x5/user-uJPET5fjNenSso8wCETWVNOp/img-poeWulglYW4aGtkUH8q32RP5.png?st=2024-09-17T10%3A30%3A54Z&amp;se=2024-09-17T12%3A30%3A54Z&amp;sp=r&amp;sv=2024-08-04&amp;sr=b&amp;rscd=inline&amp;rsct=image/png&amp;skoid=d505667d-d6c1-4a0a-bac7-5c84a87759f8&amp;sktid=a48cca56-e6da-484e-a814-9c849652bcb3&amp;skt=2024-09-16T23%3A13%3A38Z&amp;ske=2024-09-17T23%3A13%3A38Z&amp;sks=b&amp;skv=2024-08-04&amp;sig=2DNKCAWK6NJi1zdPliAqi1MGvaxYyPhACDzNqlZmPlw%3D" width="50"/>
+  </div>
+  <div class="bottom-left-icon">
+   <img alt="Icon Image" height="50" src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-LmQ09WWGIGwOeeA4ArnRw0x5/user-uJPET5fjNenSso8wCETWVNOp/img-ukC5tBpkAjC4ixs5Yi4XIVFA.png?st=2024-09-17T10%3A30%3A50Z&amp;se=2024-09-17T12%3A30%3A50Z&amp;sp=r&amp;sv=2024-08-04&amp;sr=b&amp;rscd=inline&amp;rsct=image/png&amp;skoid=d505667d-d6c1-4a0a-bac7-5c84a87759f8&amp;sktid=a48cca56-e6da-484e-a814-9c849652bcb3&amp;skt=2024-09-16T23%3A18%3A43Z&amp;ske=2024-09-17T23%3A18%3A43Z&amp;sks=b&amp;skv=2024-08-04&amp;sig=uTaiOIG0Fg9K1EFZiBoQZjkBLSrWQBCePuarKtg5zXk%3D" width="50"/>
+  </div>
+ </body>
+</html>
